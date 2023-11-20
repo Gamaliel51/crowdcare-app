c28571717846af1212dd78afc8788255c292bdff
Gamaliel51 <miraclendukwe51@gmail.com>
2023-11-20 23:11:42 +0100

    added root element and commented out firebase message element in index.html

--- src/firebase.js
+++ src/firebase.js
@@ -8,18 +8,6 @@ import { initializeAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-<<<<<<< HEAD
-const firebaseConfig = {
-    apiKey: "AIzaSyAPbi5-xdX8s3h7aK_0S38z1VjY4kfGSk4",
-    authDomain: "crowd-care.firebaseapp.com",
-    databaseURL: "https://crowd-care-default-rtdb.firebaseio.com",
-    projectId: "crowd-care",
-    storageBucket: "crowd-care.appspot.com",
-    messagingSenderId: "457828323552",
-    appId: "1:457828323552:web:71b6458d2dea63b0a68085",
-    measurementId: "G-HX6P5JD5Y9"
-  };
-=======
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -33,7 +21,6 @@ const firebaseConfig = {
   appId: "1:457828323552:web:71b6458d2dea63b0a68085",
   measurementId: "G-HX6P5JD5Y9",
 };
->>>>>>> 763d3c609841940bed2339532f8ddbda3377544c
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
